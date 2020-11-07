326cbd7ceeaacbe19c14b4e84fd9874dedab9e1e
Alona <aionovaionova@gmail.com>
2020-11-07 21:21:05 +0200

    Updates

---
+++ precache-manifest.6e99214d3c65962a7156d73bc087e1ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5aceed576bb5a86611ed99f4b6d9009",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e65037e816794874808",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "6521c3e0eba6901193ce",
+    "url": "./static/css/main.60f057a1.chunk.css"
+  },
+  {
+    "revision": "0e65037e816794874808",
+    "url": "./static/js/2.8f9a88b0.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.8f9a88b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6521c3e0eba6901193ce",
+    "url": "./static/js/main.966538ed.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
